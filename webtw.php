60c948fa42a264bcb95326dcf0111aea7ee5cb27
lm9 <depkey98@gmail.com>
2016-05-18 01:08:22 +0900

    ketu

--- webtw.php
+++ webtw.php
@@ -8,10 +8,9 @@
 * change password : https://twitter.com/settings/password/update
 */
 
-
 class webtw{
 
-	private $authenticity_token;
+	public $authenticity_token;
 	private $cookie;
 
 	function __construct($screen_name,$password){
@@ -31,44 +30,6 @@ class webtw{
 		while (!preg_match('<input type="hidden" value="([^"]++)" name="authenticity_token">', curl_exec($ch), $matches));
 		$this->authenticity_token = $matches[1];
 	}
-
-	private function Login($screen_name,$password){
-		$ch = curl_init('https://twitter.com/sessions');
-		curl_setopt_array($ch, array(
-			CURLOPT_RETURNTRANSFER => true,
-			CURLOPT_SSL_VERIFYPEER => false,
-			CURLOPT_COOKIEFILE => $this->cookie,
-			CURLOPT_COOKIEJAR => $this->cookie,
-			CURLOPT_POST    => true,
-			CURLOPT_POSTFIELDS  => http_build_query(array(
-				'session[username_or_email]'    => $screen_name,
-				'session[password]'     => $password,
-				'authenticity_token'        => $this->authenticity_token
-				))
-			));
-		curl_exec($ch);
-		$res = curl_getinfo($ch);
-		if($res["redirect_url"] != 'https://twitter.com/'){
-			throw new Exception('Login Failed', 0);
-		}
-	}
-
-	function post($url,$params, $useragent = null){
-		$params['authenticity_token'] = $this->authenticity_token;
-		$ch = curl_init($url);
-		curl_setopt_array($ch, array(
-			CURLOPT_RETURNTRANSFER => true,
-			CURLOPT_SSL_VERIFYPEER => false,
-			CURLOPT_COOKIEFILE => $this->cookie,
-			CURLOPT_COOKIEJAR => $this->cookie,
-			CURLOPT_REFERER => "https://twitter.com/",
-			CURLOPT_USERAGENT => $useragent,
-			CURLOPT_POST => true,
-			CURLOPT_POSTFIELDS => http_build_query($params)
-			));
-		return json_decode(curl_exec($ch));
-	}
-
 	function curlPost($url, $params, $useragent = null){
 		$params['authenticity_token'] = $this->authenticity_token;
 		$ch = curl_init($url);
@@ -83,5 +44,26 @@ class webtw{
 			CURLOPT_POSTFIELDS => http_build_query($params)
 			));
 		return $ch;
+	}
+
+	private function Login($screen_name,$password){
+		$ch = $this->curlPost("https://twitter.com/sessions", [
+			"session[username_or_email]" => $screen_name,
+			"session[password]" => $password,
+			"authenticity_token" => $this->authenticity_token
+			]);
+		curl_exec($ch);
+		$res = curl_getinfo($ch);
+		if($res["redirect_url"] != 'https://twitter.com/'){
+			throw new Exception('Login Failed', 0);
+		}
+	}
+
+	function post($url,$params, $useragent = null){
+		return json_decode(curl_exec($this->curlPost($url,$params,$useragent)));
+	}
+
+	function cookie(){
+		return $this->cookie;
 	}
 }
